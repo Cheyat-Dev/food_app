c103c797eed3b6a2dff0972d468f0a84070c458d
Saphal Poudyal <poudyalCheyat@gmail.com>
2020-11-20 22:56:16 +0545

    Some changes

--- food_app/lib/Pages/home.dart
+++ food_app/lib/Pages/home.dart
@@ -6,10 +6,58 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
+  List<String> recommendedItemsMock = [
+    'Coffee',
+    'Briyani',
+    'Tea',
+    'Rice',
+    'Chicken Tandoori'
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).primaryColor,
+        elevation: 0.0,
+        title: Text(
+          'Food App',
+          style: TextStyle(
+            fontWeight: FontWeight.bold,
+            fontSize: 50.0,
+            letterSpacing: 1.5,
+            fontFamily: 'BigShoulders',
+          ),
+        ),
+      ),
+      body: Container(
+        color: Theme.of(context).primaryColor,
+        child: Column(
+          children: <Widget>[
+            SizedBox(height: 50.0),
+            Container(
+              padding: EdgeInsets.symmetric(
+                horizontal: 20.0,
+                vertical: 15.0,
+              ),
+              height: 150.0,
+              color: Colors.amberAccent,
+              child: ListView.builder(
+                scrollDirection: Axis.horizontal,
+                itemCount: recommendedItemsMock.length,
+                itemBuilder: (context, index) => Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 5.0),
+                  child: Container(
+                    width: 120.0,
+                    color: Theme.of(context).accentColor,
+                    child: Text(recommendedItemsMock[index]),
+                  ),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
--- food_app/lib/main.dart
+++ food_app/lib/main.dart
@@ -4,6 +4,10 @@ import 'Pages/home.dart';
 void main(List<String> args) {
   runApp(MaterialApp(
     debugShowCheckedModeBanner: false,
+    theme: ThemeData(
+      primaryColor: Color(0xFFF2E3BC),
+      accentColor: Color(0xFF96BBBB),
+    ),
     home: Home(),
   ));
 }
